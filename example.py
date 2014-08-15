e736e3930df7e0cd7b5ccaf673f88942a4552214
HallEffect <alexander.teleshov@gmail.com>
2014-08-15 10:45:44 +0400

    - Добавил модуль построения графиков

--- example.py
+++ example.py
@@ -1,35 +1,8 @@
 # -*- coding:utf-8 -*-
 
-import pylab  # Для графиков
-from numpy import * 
 from Sender import *
 from Receiver import *
-
-# ################  Определение функции построения графиков  #####################
-
-def plot_signal(x, y, title, labelx, labley, position):
-    """Построение графика сигнала
-
-    Принимаемые параметры:
-    x - список значений аргументов
-    y - список значений функции (Внимание: len(x) == len(y))
-    title - название графика
-    labelx - подпись оси X
-    labley - подпись оси Y
-    position - позиция графика в поле окна. Что бы графики не накладывались
-    друг на друга, оставляется между графиками простанство, передвая в качестве
-    аргумента <position> нечетные значения: 1, 3, 5. Измените значение константы
-    TOTAL_QUANTITY, которая отвечает за максимальное количество графиков:
-    (TOTAL_QUANTITY >= <position>)
-
-    """
-    TOTAL_QUANTITY = 7
-    pylab.subplot(TOTAL_QUANTITY, 1, position)
-    pylab.plot(x, y)
-    pylab.title(title)
-    pylab.xlabel(labelx)
-    pylab.ylabel(labley)
-    pylab.grid(True)
+from Plot import *
 
 ##########################################################
 #
